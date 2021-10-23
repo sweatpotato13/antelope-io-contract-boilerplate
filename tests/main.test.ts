0fc08889a18c6590fd7c4d4aba1161efc9076d74
JangChorong <sweatpotato13@gmail.com>
2021-10-23 12:47:25 +0900

    feat: ✨ add test code using eosjs

---
+++ tests/main.test.ts
@@ -0,0 +1,22 @@
+import { JsonRpc, Api } from "eosjs";
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
+import fetch from 'cross-fetch';
+import { TextDecoder, TextEncoder } from "text-encoding";
+import { expect } from "chai";
+
+describe("main contract test", () => {
+    const rpc = new JsonRpc('http://localhost:8888', { fetch });
+    const api = new Api({
+        rpc,
+        signatureProvider: new JsSignatureProvider([]),
+        textDecoder: new TextDecoder(),
+        textEncoder: new TextEncoder(),
+    });
+
+    describe("blockchain connection test", async () => {
+        it("connect blockchain", async () => {
+            const result = await rpc.get_info();
+            expect(result.server_version_string).eq("v2.0.7");
+        });
+    });
+});
